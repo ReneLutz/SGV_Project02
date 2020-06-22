c8fa9e299adc5b78317cd40e5e7eb0bceabd8e15
Rene <rene.lutz@stud.tu-darmstadt.de>
2020-06-22 14:58:05 +0200

    Added scriptable objects

---
+++ Lutz_Rene_Uebung_08/Assets/Scripts/Logic/Skill.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableObject", menuName = "Skill", order = 1)]
+public class Skill : ScriptableObject
+{
+    public int Damage;
+
+    public float Range;
+
+    public int ProjectileSpeed;
+
+    public Projectile ProjectilePrefab;
+
+    public Sprite Icon;
+}
