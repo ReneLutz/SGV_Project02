dd4b6fd50e79dce2eab9a751c0c8f2dce4890d83
Rene <rene.lutz@stud.tu-darmstadt.de>
2020-06-26 14:53:28 +0200

    Imported scene controller from last project. Also implemented a WaveTracker model class, which holds the "Highscore" information for the endscreen

---
+++ Lutz_Rene_Uebung_09/Assets/Scripts/Controller/SceneController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController SceneControl { get; private set; } = null;
+
+    private enum Scenes
+    {
+        MAIN_MENU = 0,
+        GAME_SCENE = 1,
+        END_SCENE = 2
+    }
+
+    private void Awake()
+    {
+        if (SceneControl == null)
+        {
+            SceneControl = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SwitchToGameScreen()
+    {
+        StartCoroutine(SwitchScene(Scenes.GAME_SCENE));
+    }
+
+    public void SwitchToMainMenuScreen()
+    {
+        StartCoroutine(SwitchScene(Scenes.MAIN_MENU));
+    }
+
+    public void SwitchToEndScreen()
+    {
+        StartCoroutine(SwitchScene(Scenes.END_SCENE));
+    }
+
+    public void CloseGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    private IEnumerator SwitchScene(Scenes sceneEnum)
+    {
+        // From Unity SceneManager Api sample code
+        // https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.MoveGameObjectToScene.html
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync((int) sceneEnum, LoadSceneMode.Single);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Lutz_Rene_Uebung_09/Assets/Scripts/Model/WaveTracker.cs
@@ -0,0 +1,16 @@
+﻿internal static class WaveTracker
+{
+    internal static int CurrentWaveCount;
+    internal static int MaxWaveCount;
+
+    internal static int CurrentEnemyCount;
+    internal static int MaxEnemyCount;
+
+    internal static void SetupWaveStats(int currentWave, int maxWave, int currentEnemyCount, int maxEnemyCount)
+    {
+        CurrentWaveCount = currentWave;
+        MaxWaveCount = maxWave;
+        CurrentEnemyCount = currentEnemyCount;
+        MaxEnemyCount = maxEnemyCount;
+    }
+}
