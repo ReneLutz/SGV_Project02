7c3326cc33f2ef19a117ec34c367ee8c45fc15ce
Rene <rene.lutz@stud.tu-darmstadt.de>
2020-06-27 14:51:55 +0200

    Added a MainMenu Scene and imported ButtonListener class from last project.
    The ButtonListener handles the button triggered scene transitions.

---
+++ Lutz_Rene_Uebung_09/Assets/Scripts/UI/ButtonListener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ButtonListener : MonoBehaviour
+{ 
+    public void ToMainMenu()
+    {
+        SceneController.SceneControl.SwitchToMainMenuScreen();
+    }
+
+    public void ToGame()
+    {
+        SceneController.SceneControl.SwitchToGameScreen();
+    }
+
+    public void ExitGame()
+    {
+        SceneController.SceneControl.CloseGame();
+    }
+}
